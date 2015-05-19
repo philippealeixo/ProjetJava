016831be89dd61db9ed43fd0447575bdb34b3a67
GaetanCousin <cousin.gaetan@laposte.net>
2015-05-19 17:36:42 +0200

    ajout des films + modif vue

--- src/Vue/Vue.java
+++ src/Vue/Vue.java
@@ -15,25 +15,36 @@ public class Vue extends JPanel {
 
     public static final String AJOUTER = "ajouter";
     public static final String SUPPRIMER = "supprimer";
+    public static final String NOTER = "noter";
+
+
+    String[] optionTrier = { "Trie par note", "Trie par annee", "Trie par titre"}; // définition des options de la liste déroulante
+    JComboBox trier = new JComboBox(optionTrier); // Creation de la liste déroulante
+    //trier.setSelectedIndex(0);
+
+
 
     JPanel gauche;
+    JPanel boutons; // JPabel composé de Bouton
+    JPanel droite;
 
-
+    JLabel titreD;
     JLabel titreG;
     JLabel information;
+
     GestionnaireFilms gestion;
     JButton ajouter;
     JButton supprimer;
+    JButton noter;
+    JCheckBox pasVu;
+    JCheckBox vu;
 
-    JPanel boutons;
-    JPanel droite;
-    JLabel titreD;
+
+
     @SuppressWarnings("rawtypes")
     DefaultListModel listeTitre;
     @SuppressWarnings("rawtypes")
     JList liste;
-    //VueAjout vueAjout=new VueAjout();
-
 
     @SuppressWarnings({"unchecked", "rawtypes"})
     public Vue(GestionnaireFilms gestion) {
@@ -51,8 +62,12 @@ public class Vue extends JPanel {
         liste = new JList(listeTitre);
         ajouter = new JButton("Ajouter");
         supprimer = new JButton("Supprimer");
+        noter = new JButton("Noter");
         ajouter.setActionCommand(AJOUTER);
         supprimer.setActionCommand(SUPPRIMER);
+        noter.setActionCommand(NOTER);
+
+
 
         information = new JLabel();
         information.setVerticalAlignment(SwingConstants.TOP);
@@ -60,8 +75,10 @@ public class Vue extends JPanel {
         information.setOpaque(true);
 
         boutons = new JPanel();
+        boutons.add(noter);
         boutons.add(ajouter);
         boutons.add(supprimer);
+        boutons.add(trier);
         gauche.setLayout(new BorderLayout());
         gauche.add(titreG, BorderLayout.PAGE_START);
         gauche.add(liste, BorderLayout.CENTER);
@@ -83,6 +100,7 @@ public class Vue extends JPanel {
         ajouter.setEnabled(true);
         supprimer.setEnabled(false);
         ajouter.setActionCommand("ajouter");
+        noter.setEnabled(false);
     }
 
 
@@ -91,18 +109,28 @@ public class Vue extends JPanel {
     public void setControler(Controleur c) {
         liste.addListSelectionListener(c);
         ajouter.addActionListener(c);
+        trier.addActionListener(c);
        // supprimer.addActionListener(c);
 
     }
 
-public void maj() {
+    public void maj() {
+	//information.setText(gestion.getInfosHTML());
+
         information.setIcon(new ImageIcon(gestion.getImage(liste.getSelectedIndex())));
         information.setText(gestion.getInfosHTML(liste.getSelectedIndex()));
 }
 
+    public void majButton(){
+        noter.setEnabled((liste.getSelectedIndex()!=0));
+        supprimer.setEnabled((liste.getSelectedIndex()!=0));
+
+    }
+
 
 
     public int getIndex() {
+
         return liste.getSelectedIndex();
     }
 }
